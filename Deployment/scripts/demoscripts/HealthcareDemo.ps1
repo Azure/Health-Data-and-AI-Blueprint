4e365d8213ee7f0fcd511505ad6ea8a839eb0e11
Frank Simorjay <frasim@microsoft.com>
2018-01-16 09:45:31 -0800

    Updated comments, and on screen messages

--- Deployment/scripts/demoscripts/HealthcareDemo.ps1
+++ Deployment/scripts/demoscripts/HealthcareDemo.ps1
@@ -1,40 +1,89 @@
-#
-# HealthcareDemo.ps1
-# 
-# performs hospital patient data ingestion, patient admission and discharge operations.
-#
+<#
+.SYNOPSIS
+This script is designed to provide the demo capabilities of the blueprint solution. The code located here is designed to help understand how you can securely upload a built data set, and import patient data for ML analysis and storage.
+
+.DESCRIPTION
+
+FHIR (Fast Healthcare Interoperability Resources) is a specification for exchanging healthcare data in a modern and developer friendly way.
+FHIR Schemas used in the Healthcare Solution Blueprint
+The current solution is made up of the following sample FHIR Schema -
+	• Patient schema link - https://www.hl7.org/fhir/patient.html
+	• Observation schema link - https://www.hl7.org/fhir/observation.html
+	• Encounter schema link - https://www.hl7.org/fhir/encounter.html
+	• Condition schema link - https://www.hl7.org/fhir/condition.html
+
+Copyright (c) Microsoft Corporation and Avyan Consulting Corp. All rights reserved.
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is  furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  FITNESS FOR A PARTICULAR PURPOSE AND ONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+.EXAMPLE
+
+.\HealthcareDemo.ps1 -deploymentPrefix <deployment-prefix> -Operation Ingestion
+
+To input the sample historical patient data into the solution.
+
+.EXAMPLE
+
+.\HealthcareDemo.ps1 -deploymentPrefix <deployment-prefix> -Operation BulkPatientAdmission
+
+Adds newly admitted patients to the database by uploading their information in FHIR format.
+
+.EXAMPLE
+
+.\HealthcareDemo.ps1 -deploymentPrefix <deployment-prefix> -Operation BulkPatientDischarge
+
+Discharges patients to the database by uploading discharge information in FHIR format.
+
+#>
 
 [CmdletBinding()]
 param
 (
+	#Deployment prefix of the deployed solution
     [Parameter(Mandatory = $true)]
     [Alias("prefix")]
     [ValidateLength(1,5)]
     [ValidatePattern("[a-zA-Z][a-zA-Z0-9]")]
 	[string]$deploymentPrefix,
 
+	#Healthcare operation (ingestion/bulk admission/bulk discharge)
 	[parameter(Mandatory=$true)]
 	[ValidateSet("Ingestion","BulkPatientAdmission","BulkPatientDischarge")]
 	[string]$Operation
 )
 
-# globals
+# global variables
 
+#script path
 $scriptPath = Split-Path $MyInvocation.MyCommand.Path
+
+#import dependent module
 Import-Module "$scriptPath/lib.psm1"
+
+#script root
 $scriptRoot = Split-Path (Split-Path ( Split-Path $MyInvocation.MyCommand.Path ))
+
+#Sample FHIR defined patient status.
 $clinicalStatus="active","recurrence" ,"inactive" , "remission", "resolved"
+
+#Sample FHIR defined gender
 $gender="male","female"
+
+#Sample dates used for patient admission
 $vDates=5,7,10,7,12,13
+
+#Deployment prefix passed during the deployment
 $deploymentprefix = $deploymentprefix.ToLower()
 
 switch($Operation){
 	Ingestion{
+				# The LengthOfStayname.csv provides a dataset used for the experiments throughout this Blueprint. Replacing the csv will not guarantee that importing of data will succeed.
                 if (Test-Path "$scriptroot\trainingdata\LengthOfStayname.csv" ) {
                     $blobPath = "$scriptroot\trainingdata\LengthOfStayname.csv"
                 }
                 else {
-                    Write-Host "`nFailed to find LOS Training Data CSV file. Please enter valid path for CSV" -ForegroundColor Red
+                    Write-Host "`nFailed to find LOS Training Sample Data CSV file. Please enter valid path for CSV" -ForegroundColor Red
                     $blobPath = Read-Host "Enter File Path"
                 }
                 ### Create PSCredential Object for GlobalAdmin Account
@@ -44,20 +93,20 @@ switch($Operation){
                 Write-Host -ForegroundColor Yellow "`nConnecting to AzureRM Subscription using $($deploymentOutput.UPN_DataScientistUser) Account."
                 $azureRMContext = Login-AzureRmAccount -Credential $credential -ErrorAction SilentlyContinue
                 if($azureRMContext-ne $null){
-                    Write-Host -ForegroundColor Green "`nConnection was successful." 
+                    Write-Host -ForegroundColor Green "`nConnected." 
                 }
                 Else{
-                    Write-Host -ForegroundColor Red "`nFailed connecting to Azure." 
+                    Write-Host -ForegroundColor Red "`nConnection failed." 
                     break
                 }
                 try {
-                    Write-Host -ForegroundColor Yellow "`nGetting StorageAccessKey for Storage Account - $($deploymentOutput.BlobStorageAccountName)"
+                    Write-Host -ForegroundColor Yellow "`nRetrieving StorageAccessKey for Storage Account - $($deploymentOutput.BlobStorageAccountName)"
                     $storageAccessKey = (Get-AzureRmStorageAccount | Where-Object StorageAccountName -eq $deploymentOutput.BlobStorageAccountName | Get-AzureRmStorageAccountKey)[0].Value
                     Write-Host -ForegroundColor Yellow "`nCreating Azure storage context to upload LOS data."
-                    $storageContext = New-AzureStorageContext -StorageAccountName $deploymentOutput.BlobStorageAccountName -StorageAccountKey $storageAccessKey
-                    Write-Host -ForegroundColor Yellow "`nUploading LOS data.."
+					$storageContext = New-AzureStorageContext -StorageAccountName $deploymentOutput.BlobStorageAccountName -StorageAccountKey $storageAccessKey
+					Write-Host "Connecting to $($deploymentOutput.BlobStorageAccountName). Uploading Sample CSV data to storage container - 'trainingdata'"
                     Set-AzureStorageBlobContent -File $blobPath -Container 'trainingdata' -Context $storageContext
-                    Write-Host -ForegroundColor Yellow "`nLOS data uploaded successfully. "
+                    Write-Host -ForegroundColor Yellow "`nSample CSV data uploaded."
                 }
                 catch {
                      Write-Host "error : $_"
@@ -65,32 +114,36 @@ switch($Operation){
 	}
 	BulkPatientAdmission{
 		try {
-				Write-Host "getting app details..." -Foregroundcolor Yellow
+				Write-Host "Retrieving installation configuration from deploymentoutput.json" -Foregroundcolor Yellow
 				$deploymentOutput = Get-Content -Path "$scriptroot\output\$($deploymentPrefix)-deploymentOutput.json" | ConvertFrom-Json
 				
-				Write-Host "getting data format..." -Foregroundcolor Yellow
+				Write-Host "Reading \scripts\jsonscripts\input_admission.json" -Foregroundcolor Yellow
 				$patientInput = 
 					Get-Content -Path $scriptroot\scripts\jsonscripts\input_admission.json | ConvertFrom-Json
 
-				Write-Host "please login..." -Foregroundcolor DarkGray
+                #Login using Chris_CareLineManager and obtain authentication token from Get-AuthToken in .\lib.psm1
+				Write-Host "Pop-up login dialog box requires attention." -Foregroundcolor Yellow
+				Write-Host "Use username - Chris_CareLineManager@<DOMAIN> and password used during setup.  Refer to '\output\<deploymentPrefix>-deploymentOutput.json' for deployment password. Ensure permissions have been set correctly; see FAQ ‘access_denied’ Setting for correct use. " -Foregroundcolor Yellow
 				$token = Get-AuthToken -tenantId $deploymentOutput.AADTenantId -clientId $deploymentOutput.AADApplicationClientId -clientSecret $deploymentOutput.DeploymentPassword -replyUrl $deploymentOutput.AADAppReplyUrl
 
-				Write-Host "obtained authentication token.." -ForegroundColor Yellow
+				Write-Host "Obtained authentication token." -ForegroundColor Yellow
 
+				#Get patient admission json files \scripts\demoscripts\admit_10000x.json
 				$files = (Get-ChildItem admit -Filter "*.json").FullName
 
+                #Import 10 sample patients into solution
 				$countAdmitted = 0
 				$fhir=""
 				$output=""
 				$fhirJson=""
 				$vcount=0
 				foreach($file in $files){
-
 					$fhir = (Get-Content -Path $file) | ConvertFrom-Json
 					$fhir.encounter.period.start=[DateTime]::Now.AddDays(-1*$vDates[$vcount]).ToString("yyyy-MM-ddThh:mm:sszzz")
 					$fhirJson=$fhir | ConvertTo-Json -Depth 5
+					#call admission azure function over rest using input data $fhirJson and access token
+					Write-Host "Calling $($deploymentOutput.AzFunc_AdmissionFunctionUrl) and POSTing data for $file"
 					$output = Invoke-HealthcareFunction -accessToken $token -httpMethod Post -data $fhirJson -functionUrl $deploymentOutput.AzFunc_AdmissionFunctionUrl
-					
 					if($output.result -ne "invalid encounter id" -and $output.result -ne "failed"){
 						$countAdmitted++
 					}
@@ -98,29 +151,33 @@ switch($Operation){
 					$vcount++
 				}
 
-				Write-Host "patient admitted count : $countAdmitted" -ForegroundColor DarkCyan
+				Write-Host "Total patient admitted : $countAdmitted" -ForegroundColor DarkCyan
              }
 		catch {
-				 Write-Host "error : $_"
+				 Write-Host "error : $_" -ForegroundColor Red
 			}
 	}
 	BulkPatientDischarge{
 		 try {
 
-                Write-Host "getting app details..." -Foregroundcolor Yellow
+                Write-Host "Retrieving installation configuration from deploymentoutput.json" -Foregroundcolor Yellow
                 $deploymentOutput = Get-Content -Path "$scriptroot\output\$($deploymentPrefix)-deploymentOutput.json" | ConvertFrom-Json
 			
-				Write-Host "getting data format..." -Foregroundcolor Yellow
+				Write-Host "Reading \scripts\jsonscripts\input_discharge.json" -Foregroundcolor Yellow
 				$patientInput = 
 					Get-Content -Path $scriptroot\scripts\jsonscripts\input_discharge.json | ConvertFrom-Json
 				
-				Write-Host "please login..." -Foregroundcolor DarkGray
+                #Login using Chris_CareLineManager and obtain authentication token from Get-AuthToken in .\lib.psm1
+				Write-Host "Pop-up login dialog box requires attention." -Foregroundcolor Yellow
+				Write-Host "Use username - Chris_CareLineManager and deployment password, refer to '\output\<deploymentPrefix>-deploymentOutput.json for deployment password." -Foregroundcolor DarkGray
 				$token = Get-AuthToken -tenantId $deploymentOutput.AADTenantId -clientId $deploymentOutput.AADApplicationClientId -clientSecret $deploymentOutput.DeploymentPassword -replyUrl $deploymentOutput.AADAppReplyUrl
 				
-				Write-Host "obtained authentication token..." -Foregroundcolor Yellow
+				Write-Host "Obtained authentication token." -ForegroundColor Yellow
 
+                #Get patient admission json files \scripts\demoscripts\discharge_10000x.json
 				$files = (Get-ChildItem discharge -Filter "*.json").FullName
 
+                #Discharge 6 sample patients from solution
 				$countDischarged = 0
 				$fhir=""
 				$output=""
@@ -130,6 +187,9 @@ switch($Operation){
 					$fhir = (Get-Content -Path $file) | ConvertFrom-Json
 					$fhir.encounter.period.end = [DateTime]::Now.ToString("yyyy-MM-ddThh:mm:sszzz")
 					$fhirJson = $fhir | ConvertTo-Json -Depth 5
+
+					#call discharge azure function over rest using input data $fhirJson and access token
+					Write-Host "Calling $($deploymentOutput.AzFunc_DischargeFunctionUrl) and POSTing data for $file"
 					$output = Invoke-HealthcareFunction -accessToken $token -httpMethod Put -data $fhirJson -functionUrl $deploymentOutput.AzFunc_DischargeFunctionUrl
 					
 					if($output.result -ne "invalid encounter id" -and $output.result -ne "failed"){
@@ -137,7 +197,7 @@ switch($Operation){
 					}
 				}
 
-				Write-Host "patient discharged count : $countDischarged" -ForegroundColor DarkCyan
+				Write-Host "Total patient discharged : $countDischarged" -ForegroundColor DarkCyan
              }
          catch {
                  Write-Host "error : $_" -ForegroundColor Red
--- Deployment/scripts/demoscripts/lib.psm1
+++ Deployment/scripts/demoscripts/lib.psm1
@@ -1,31 +1,50 @@
-#
-# lib.psm1
-#
-# Contains functions for healthcare operations
-#
+<#
+.SYNOPSIS
+This script is designed to provide the demo capabilities of the blueprint solution. The code located here is designed to help understand how you can securely upload a built data set, and import patient data for ML analysis and storage.
 
-# Get Authentication token
+.DESCRIPTION
+
+Copyright (c) Microsoft Corporation and Avyan Consulting Corp. All rights reserved.
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is  furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  FITNESS FOR A PARTICULAR PURPOSE AND ONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+.EXAMPLE
+
+Import-Module -Name <ModulePath>\lib.psm1
+
+Imports module in the session.
+#>
+
+# Get Authentication token, Opening up a browser instance to prompt for user login and tokenising credentials.
 function Get-AuthToken{
     param(
+		
+		#Azure AD Tenant Id.
         [parameter(Mandatory=$true)]
         [string]$tenantId,
+
+		#Azure AD Application Client Id
         [parameter(Mandatory=$true)]
         [string]$clientId,
+
+		#Azure AD Application Client Secret
         [parameter(Mandatory=$true)]
         [string]$clientSecret,
+
+		#Azure AD Application Reply Url
         [parameter(Mandatory=$true)]
         [string]$replyUrl
 		)
 	process{
 
-		# Authorize
+		# Microsoft login Authorize URL
         $authorzationUrl = "https://login.microsoftonline.com/$tenantId/oauth2/authorize?response_type=code&client_id=$clientId&redirect_uri=$replyUrl&resource=$clientId"
         
-        # Load UI
-        # Fake a proper endpoint for the Redirect URI
+        # Login browser 
         $code = LoginBrowser $authorzationUrl (New-Object system.uri($replyUrl))
 
-		  # Access token
+		# User OAuth token retrieval.
         $ClientSecret=[System.Web.HttpUtility]::UrlEncode($ClientSecret)
         $tokenRequestBody="client_id=$clientId&redirect_uri=$replyUrl&grant_type=authorization_code&client_secret=$ClientSecret&code=$code"
         $tokenResponse=Invoke-WebRequest -Method Post -Body $tokenRequestBody -Uri https://login.microsoftonline.com/common/oauth2/token
@@ -36,16 +55,25 @@ function Get-AuthToken{
 	}
 }
 
-# Call Healthcare Functions
+<#
+.Description 
+The following function will call the custom Invoke-Healthcare funciton which is designed to authenticate an azure application for data processing.
+The function will return status on successful and failed transactions.
+#>
 function Invoke-HealthcareFunction{
 	param(
+		#user access token
 		[parameter(Mandatory=$true)]
 		[string]$accessToken,
+		#http method as per azure function need
 		[parameter(Mandatory=$true)]
 		[validateset("Get","Post","Put")]
 		[string]$httpMethod,
+		#input for post/put request data
 		[parameter(Mandatory=$true)]
 		[string]$data,
+
+		#function endpoint url
 		[parameter(Mandatory=$true)]
 		[string]$functionUrl
 	)
@@ -76,7 +104,7 @@ function Invoke-HealthcareFunction{
 	}
 }
 
-# generate reandom alphabet string of specified length
+# Generates random string based on specified length
 function Get-RandomName{
 	param(
 		[parameter(Mandatory=$true)]
@@ -89,6 +117,7 @@ function Get-RandomName{
 	}
 }
 
+#Generates random integer based on minimum and maximum input provided.
 function Get-RandomEncounterId{
 	param(
 		[parameter(Mandatory=$true)]
@@ -102,9 +131,7 @@ function Get-RandomEncounterId{
 	}
 }
 
-# display browser window and navigate to authorize url
-# for user login and obtain the authorization code after
-# successful login
+# Display browser window and navigate to authorized url for user login and obtain the authorization code after successful login.
 function LoginBrowser
 {
     param
@@ -120,7 +147,7 @@ function LoginBrowser
 
     $outputAuth = ".\Code.txt"
 
-	# Create an Internet Explorer Window for the Login Experience
+	# Create an browser dialog box for the login.
     $ie = New-Object -ComObject InternetExplorer.Application
     $ie.Width = 600
     $ie.Height = 500
@@ -134,12 +161,12 @@ function LoginBrowser
 
     :loop while($true)
     {   
-		# Grab URL in IE Window
+		# Retrieve URL in login dialog box
         $urls = (New-Object -ComObject Shell.Application).Windows() | Where-Object {($_.LocationUrl -match "(^https?://.+)|(^ftp://)") -and ($_.HWND -eq $ie.HWND)} | Where-Object {$_.LocationUrl}
 
         foreach ($a in $urls)
         {
-			# If URL is in the form we expect, with the Reply URL as the domain, and the code in the URL, grab the code
+			# Verifies the reply url which contains '?code=' and retrieve the authorisation code.
             if (($a.LocationUrl).Contains("?code="))
             {
                 $code = ($a.LocationUrl)
@@ -148,7 +175,7 @@ function LoginBrowser
                 $ie.Quit()
                 break loop
             }
-			# If we catch an error, output the error information
+			# If url reverts '?error=', catch it as an error.
 			elseif (($a.LocationUrl).StartsWith($redirectUri.ToString()+"?error="))
             {
                 $error = [System.Web.HttpUtility]::UrlDecode(($a.LocationUrl) -replace (".*error="))
